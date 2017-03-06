233cf72eb81f322d864dcc1756f7566037665681
Latios Thinh <nguyenquocthinh91@gmail.com>
2017-03-06 19:27:37 +0800

    add logo

--- wine/js/videoHover.js
+++ wine/js/videoHover.js
@@ -1,10 +1,10 @@
 var video = document.getElementById("video");
-var figure = $(".active-video a").hover(hoverVideo, hideVideo);
+//var figure = $(".active-video a").hover(pauseVideo, hoverVideo);
 var intervalRewind;
 function hoverVideo(e) {  
     //$('video', this).get(0).play();
     video.play();
-    video.playbackRate = 1.0;
+    video.playbackRate = .5;
     clearInterval(intervalRewind);
 }
 
@@ -12,13 +12,18 @@ function hideVideo(e) {
     //$('video', this).get(0).pause();
     //video.pause();
     intervalRewind = setInterval(function(){
-       video.playbackRate = 0.5;
+       video.playbackRate = 3;
        if(video.currentTime == 0){
            clearInterval(intervalRewind);
            video.pause();
        }
        else{
-           video.currentTime -= .1;
+           video.currentTime -= .3;
        }
                 },50);
 }
+
+function pauseVideo(e) {
+    video.pause();
+}
+
