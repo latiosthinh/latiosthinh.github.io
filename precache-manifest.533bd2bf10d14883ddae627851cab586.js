84421b646ac14064eaa234c9b7ea8b0191a7a419
coder054 <binhdinhgiaftu@gmail.com>
2019-02-26 19:31:10 +0700

    Updates

---
+++ precache-manifest.533bd2bf10d14883ddae627851cab586.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "dce6843a020165396895f879269db655",
+    "url": "/cat-test/static/media/iCielPanton-Light.dce6843a.otf"
+  },
+  {
+    "revision": "d010dadce55526fa0dfb",
+    "url": "/cat-test/static/css/main.eb8bd4cd.chunk.css"
+  },
+  {
+    "revision": "69d7f2b5eab465b99743",
+    "url": "/cat-test/static/js/runtime~main.69d7f2b5.js"
+  },
+  {
+    "revision": "9a9308efc0afc7c9f696",
+    "url": "/cat-test/static/js/2.9a9308ef.chunk.js"
+  },
+  {
+    "revision": "79295acba271e9145fc5f3f4aedea5dd",
+    "url": "/cat-test/static/media/footer-bg.79295acb.png"
+  },
+  {
+    "revision": "1e55721dfb826f6502b41218e979cbb2",
+    "url": "/cat-test/static/media/wave.1e55721d.png"
+  },
+  {
+    "revision": "ea090dd5bb63809b52e7b06d1b20cadb",
+    "url": "/cat-test/static/media/HoboStdMedium.ea090dd5.ttf"
+  },
+  {
+    "revision": "45d8b517871a6913c74cdd20a7c9b726",
+    "url": "/cat-test/static/media/Tw Cen MT Condensed Extra Bold.45d8b517.TTF"
+  },
+  {
+    "revision": "d010dadce55526fa0dfb",
+    "url": "/cat-test/static/js/main.d010dadc.chunk.js"
+  },
+  {
+    "revision": "387c2c354c735b21fafb3f4d612460e7",
+    "url": "/cat-test/static/media/CapricaScriptPersonalUse.387c2c35.ttf"
+  },
+  {
+    "revision": "7d2fb0da75623f1b5d9066755a6aabbb",
+    "url": "/cat-test/static/media/CapricaScriptUprightPersonalUse.7d2fb0da.ttf"
+  },
+  {
+    "revision": "cb5a21a92d77658df1beee4d51b72777",
+    "url": "/cat-test/static/media/comic.cb5a21a9.ttf"
+  },
+  {
+    "revision": "fcb8965acd0e90c50138958a2a7e0421",
+    "url": "/cat-test/static/media/TIMES.fcb8965a.TTF"
+  },
+  {
+    "revision": "460aa0de85e9e59ed111ba443f459bdf",
+    "url": "/cat-test/static/media/background-sothutu-cauhoi.460aa0de.png"
+  },
+  {
+    "revision": "ea090dd5bb63809b52e7b06d1b20cadb",
+    "url": "/cat-test/static/media/Hobo Std Medium.ea090dd5.ttf"
+  },
+  {
+    "revision": "39338481a9b2c3753b61115062caf9b4",
+    "url": "/cat-test/static/media/MYRIADPRO-REGULAR.39338481.woff"
+  },
+  {
+    "revision": "7f1c154bb54de4a4e2e6fb62259b3c19",
+    "url": "/cat-test/index.html"
+  }
+];
