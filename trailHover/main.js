34619db0f297f0f45f54fec1eeb4eca2269735e3
Latios Thinh <latiosthinh@live.com>
2019-05-17 10:13:05 +0700

    add project

--- trailHover/main.js
+++ trailHover/main.js
@@ -18,11 +18,11 @@ function CalcSize(num){
 
 
 var shape = ['fa fa-phone', 'fa fa-heart', 'fa fa-behance', 'fa fa-envelope', 'fa fa-bicycle', 'fa fa-facebook', 'fa fa-male', 'fa fa-github', 'fa fa-align-right', 'fa fa-id-card', 'fa fa-bath'];
-var link = ['', '', 'https://www.behance.net/latiosthin9651', '', '', 'https://facebook.com/thomas.4542', '', 'https://latiosthinh.github.io', '', '',''];
+var link = ['', '', 'https://www.behance.net/latiosthin9651', '', '', 'https://facebook.com/thomas.4542', '', 'https://latiosthinh.github.io', 'project', '',''];
 var divch = [$('.phone'), $('.heart'), $('.behance'), $('.mail'), $('.bike'), $('.facebook'), $('.man'), $('.github'), $('.menubar'), $('.profile'), $('.bath')];
 
 function animate(num) {
-    for( var _i = 0; _i < num; _i++) { 
+    for( var _i = 0; _i < num; _i++) {
         var rd = chance.integer({ min: 0, max: 500 });
         var dl = chance.integer({ min: 0, max: num });
         //console.log(chance.integer({ min: 0, max: num }));
@@ -44,12 +44,12 @@ function animate(num) {
         $ele.addClass(shape[_i]).append('<div class="sqc"><span class="sqc1"></span><span class="sqc2"></span><span class="sqc3"></span><span class="sqc4"></span></div><a href="'+ link[_i] +'" target=_blank></a>');
         
         tl = new TimelineMax({repeat:-1, repeatDelay: dl,})
-                                    .from($('.sq:nth-child('+ rd +') .sqc .sqc1'), 0.8, {y:100})
-                                    .from($('.sq:nth-child('+ rd +') .sqc .sqc2'), 0.8, {x:-100})
-                                    .from($('.sq:nth-child('+ rd +') .sqc .sqc3'), 0.8, {y:-100})
-                                    .from($('.sq:nth-child('+ rd +') .sqc .sqc4'), 0.8, {x:100})
-                                    .to('.sq:nth-child('+ rd +')', 1, {css: {opacity: '0.2'}})
-                                    .to('.sq:nth-child('+ rd +') .sqc', 1, {css: {opacity: '0'}});
+                .from($('.sq:nth-child('+ rd +') .sqc .sqc1'), 0.8, {y:100})
+                .from($('.sq:nth-child('+ rd +') .sqc .sqc2'), 0.8, {x:-100})
+                .from($('.sq:nth-child('+ rd +') .sqc .sqc3'), 0.8, {y:-100})
+                .from($('.sq:nth-child('+ rd +') .sqc .sqc4'), 0.8, {x:100})
+                .to('.sq:nth-child('+ rd +')', 1, {css: {opacity: '0.2'}})
+                .to('.sq:nth-child('+ rd +') .sqc', 1, {css: {opacity: '0'}});
         $ele.append(divch[_i]);
         // if($('.sq:nth-child('+ rd +')').length) {
         var _top = $ele.offset().top;
@@ -74,14 +74,21 @@ function animate(num) {
 
 function splashScreen() {
     tl = new TimelineMax()
-                                .fromTo('.spl1', 0.5, {y:0}, {y:-52}, 0)
-                                .fromTo('.spl3', 0.5, {y:0}, {y:-52}, 0)
-                                .fromTo('.spl4', 0.5, {y:0}, {y:-52}, 0)
-                                .fromTo('.spl2', 0.5, {y:0}, {y:52}, '+=0.5')
-                                .fromTo('.spl3', 0.5, {x:0}, {x:-52}, '+=0.5')
-                                .fromTo('.spl4', 0.5, {x:0}, {x:52}, '+=0.5')
-                                .fromTo('.outer1', 0.8, {css: {opacity: '0'}}, {css: {opacity: '1'}}, '+=0.5')
-                                .fromTo('.outer1', 0.8, {css: {transform: 'rotate(0deg)'}}, {css: {transform: 'rotate(45deg)'}})
-                                //.addPause(5)
+            .fromTo('.spl1', 0.5, {y:0}, {y:-52}, 0)
+            .fromTo('.spl3', 0.5, {y:0}, {y:-52}, 0)
+            .fromTo('.spl4', 0.5, {y:0}, {y:-52}, 0)
+            .fromTo('.spl2', 0.5, {y:0}, {y:52}, '+=0.5')
+            .fromTo('.spl3', 0.5, {x:0}, {x:-52}, '+=0.5')
+            .fromTo('.spl4', 0.5, {x:0}, {x:52}, '+=0.5')
+            .fromTo('.outer1', 0.8, {css: {opacity: '0'}}, {css: {opacity: '1'}}, '+=0.5')
+            .fromTo('.outer1', 0.8, {css: {transform: 'rotate(0deg)'}}, {css: {transform: 'rotate(45deg)'}})
+            //.addPause(5)
 
 }
+
+
+$(document).ready(function(){
+    $("[href=project]").hover(() => { $('#project').addClass('open') })
+    $('.close').click(() => { $('#project').removeClass('open') })
+});
+
