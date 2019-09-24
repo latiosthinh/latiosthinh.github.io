126b24be0546acf7f3b59c120b2989041be9a4a8
Latios Thinh <latiosthinh@live.com>
2019-09-24 11:34:18 +0700

    zone 2

--- vincom/js/script.js
+++ vincom/js/script.js
@@ -1,3 +1,46 @@
+$( document ).ready( ()=> {
+	var c4k4 = document.getElementById( '_x34_c4k' )
+	var bank = document.getElementById( 'bank' )
+	var wall = document.getElementById( 'wall' )
+	var twinTower = document.getElementById( 'twin-tower' )
+	var playGround = document.getElementById( 'play-ground' )
+	var headquater = document.getElementById( 'headquater' )
+	var museum = document.getElementById( 'museum' )
+	var show = document.getElementById( 'show' )
+	var sidehouse = document.getElementById( 'side-house' )
+	var church = document.getElementById( 'church' )
+	var cornerhouse = document.getElementById( 'corner-house' )
+
+	// setup scene
+	var setupTL = new TimelineMax()
+		setupTL
+			.to( c4k4, 0, { transformStyle:"preserve-3d", scale:0, ease:Cubic.easeOut } )
+			.to( museum, 0, { transformStyle:"preserve-3d", scale:0, ease:Cubic.easeOut } )
+			.to( bank, 0, { transformStyle:"preserve-3d", scale:0, ease:Cubic.easeOut } )
+			.to( wall, 0, { transformStyle:"preserve-3d", scale:0, ease:Cubic.easeOut } )
+			.to( twinTower, 0, { transformStyle:"preserve-3d", scale:0, ease:Cubic.easeOut } )
+			.to( headquater, 0, { transformStyle:"preserve-3d", scale:0, ease:Cubic.easeOut } )
+			.to( playGround, 0, { transformStyle:"preserve-3d", scale:0, ease:Cubic.easeOut } )
+			.to( show, 0, { transformStyle:"preserve-3d", scale:0, ease:Cubic.easeOut } )
+			.to( sidehouse, 0, { transformStyle:"preserve-3d", scale:0, ease:Cubic.easeOut } )
+			.to( church, 0, { transformStyle:"preserve-3d", scale:0, ease:Cubic.easeOut } )
+			.to( cornerhouse, 0, { transformStyle:"preserve-3d", scale:0, ease:Cubic.easeOut } )
+
+	setTimeout(() => {
+		var firstTL = new TimelineMax()
+		firstTL
+			.to( c4k4, 0.2, { scale:1, ease:Cubic.easeOut } )
+			.to( museum, 0.2, { scale:1, ease:Cubic.easeOut } )
+			.to( bank, 0.2, { scale:1, ease:Cubic.easeOut } )
+			.to( wall, 0.2, { scale:1, ease:Cubic.easeOut } )
+			.to( twinTower, 0.2, { scale:1, ease:Cubic.easeOut } )
+			.to( headquater, 0.2, { scale:1, ease:Cubic.easeOut } )
+			.to( playGround, 0.2, { scale:1, ease:Cubic.easeOut } )
+			.to( show, 0.2, { scale:1, ease:Cubic.easeOut } )
+			.to( sidehouse, 0.2, { scale:1, ease:Cubic.easeOut } )
+			.to( church, 0.2, { scale:1, ease:Cubic.easeOut } )
+			.to( cornerhouse, 0.2, { scale:1, ease:Cubic.easeOut } )
+	}, 3000);
 $(function () { // wait for document ready
 	var flightpath = {
 		entry : {
@@ -27,7 +70,7 @@ $(function () { // wait for document ready
 			values: [
 					{x: 660,	y: 20},
 					{x: 800,	y: 130},
-					{x: $(window).width() + 300,	y: -100},
+					{x: $(window).width() + 300,	y: 1000},
 				]
 		}
 	};
@@ -38,16 +81,60 @@ $(function () { // wait for document ready
 	var tween = new TimelineMax()
 		.add(TweenMax.to($("#plane"), 1.2, {css:{bezier:flightpath.entry}, ease:Power1.easeInOut}))
 		.add(TweenMax.to($("#plane"), 2, {css:{bezier:flightpath.looping}, ease:Power1.easeInOut}))
-		.add(TweenMax.to($("#plane"), 1, {css:{bezier:flightpath.leave}, ease:Power1.easeInOut}));
+		.add(TweenMax.to($("#plane"), 1, {css:{bezier:flightpath.leave}, ease:Power1.easeInOut}))
+		.play();
 
 	// build scene
-	var scene = new ScrollMagic.Scene({triggerElement: "#trigger", duration: 5000, offset: 100})
+	var scene = new ScrollMagic.Scene({
+		triggerElement: "#trigger", duration: 5000, offset: 100
+	})
 					.setPin("#target")
 					.setTween(tween)
-					.addIndicators() // add indicators (requires plugin)
+					// .addIndicators() // add indicators (requires plugin)
 					.addTo(controller);
 
 	document.addEventListener( 'mousemove', (e) => {
-		console.log(e.clientX + ' , ' + e.clientY)
+		// console.log(e.clientX + ' , ' + e.clientY)
+	} )
+
+
+	
+	// TweenMax.fromTo( c4k4, 1,
+	// 	{ 
+	// 		transformStyle:"preserve-3d",
+	// 		scale:0,
+	// 		transformOrigin:"75% 50%",
+	// 		ease:Cubic.easeOut
+	// 	},
+	// 	{
+	// 		transformStyle:"preserve-3d",
+	// 		scale:1,
+	// 		transformOrigin:"75% 50%",
+	// 		ease:Cubic.easeOut
+	// 	}
+	// )
+
+	
+
+
+	// bank.addEventListener( 'mouseover', (e)=>{
+	// 	TweenMax.to( bank, 0.3, { 
+	// 		transformStyle:"preserve-3d",
+	// 		scale:1.2,
+	// 		transformOrigin:"75% 50%",
+	// 		ease:Cubic.easeOut
+	// 	} )
+	// } )
+	// bank.addEventListener( 'mouseleave', ()=> {
+	// 	TweenMax.to( bank, 0.3, { 
+	// 		transformStyle:"preserve-3d",
+	// 		scale:1,
+	// 		transformOrigin:"75% 50%",
+	// 		ease:Cubic.easeOut
+	// 	} )
+	// } )
+	bank.addEventListener( 'click', ()=>{
+		console.log('asd')
 	} )
 })
+} )
